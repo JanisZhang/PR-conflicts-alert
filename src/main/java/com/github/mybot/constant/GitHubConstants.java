182ee7deb582b52c9336dbba9f707415521af2e0
Janis Zhang <janis.zhang@liferay.com>
2023-12-12 13:53:11 +0800

    Change MAX_RETRY_TIMES to ten

--- src/main/java/com/github/mybot/constant/GitHubConstants.java
+++ src/main/java/com/github/mybot/constant/GitHubConstants.java
@@ -13,7 +13,7 @@ public class GitHubConstants {
             ".com/apps/merge-conflicts-alert)";
 
     public static final String LABEL_NAME = "Merge Conflicts";
-    public static final int MAX_RETRY_TIMES = 5;
+    public static final int MAX_RETRY_TIMES = 10;
     public static final int RETRY_INTERVAL = 1000;
 }
 
